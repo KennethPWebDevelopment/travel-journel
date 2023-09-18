889cea9f9d6e85fb519f9a769c14620ed3594c2a
Kenneth Paige <KEPAIGE@atl.com>
2023-09-18 12:34:55 -0400

    capatalize data text

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ export default [
     {
         id: "1",
         title: "Mount Fuji",
-        location: "Japan",
+        location: "JAPAN",
         googleMapsUrl: "https://www.google.com/maps/place/Mount+Fuji/@35.360625,138.7273634,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA!5m1!1e4?entry=ttu",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
